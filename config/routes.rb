54a1c413049b8d5e3dac6aa228306b538ae80664
Craig <craig.coleman.cc@gmail.com>
2022-09-27 15:20:24 -0500

    add get '/me' route to 'users#show'

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :publishers, only: [:index, :create]
   resources :comicbooks
 
+  get '/me', to: "users#show"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
   
